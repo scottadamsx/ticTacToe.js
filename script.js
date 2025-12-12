24afe70ea8cacaf8115ff682825f685f9a38fe14
Scott Adams <scottadamsx@gmail.com>
2025-12-12 02:14:03 -0330

    finishing touches
    added stalemate logic and added different colors for X & O

--- script.js
+++ script.js
@@ -18,23 +18,37 @@ function switchPlayer(player) {
 }
 console.log(slots)
 
-
+function addPlayAgainBtn() {
+    const playAgainBtn = document.createElement("button")
+    playAgainBtn.textContent = "Play Again"
+    playAgainBtn.addEventListener("click", () => {
+        window.location.reload()
+    })
+    document.getElementById("playAgain").appendChild(playAgainBtn)
+}
 
 slots.forEach(slot => {
     slot.addEventListener("click", evt => {
         if (slot.textContent == "") {
+            if (player == "x") {
+                slot.style.color = "blue"
+            } else {
+                slot.style.color = "orange"
+            }
             slot.textContent = player
             
             win = checkForWin(slots, player)
             if (win == true) {
                 messageBox.textContent = `${player} has won the game!`
-                const playAgainBtn = document.createElement("button")
-                playAgainBtn.textContent = "Play Again"
-                playAgainBtn.addEventListener("click", () => {
-                    window.location.reload()
-                })
-                document.getElementById("playAgain").appendChild(playAgainBtn)
-            } else {
+                addPlayAgainBtn()
+                removeClick(slots, player)
+            } 
+            else  if (win == "stalemate") {
+                messageBox.textContent = `Stalemate... better luck next time!`
+                addPlayAgainBtn()
+                removeClick(slots, player)
+            } 
+            else {
                 player = switchPlayer(player)
             messageBox.textContent = `${player}'s turn!`
             }
@@ -57,56 +71,51 @@ function checkForWin(slots, player) {
     // if top row matches
     if (slots[0].textContent == slots[1].textContent && slots[1].textContent == slots[2].textContent && slots[0].textContent != "") {
         win = true
-        removeClick(slots, player)
-        
     }
    // if middle row matches
     else if (slots[3].textContent == slots[4].textContent && slots[4].textContent == slots[5].textContent && slots[5].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
     // if bottom matches
     else if (slots[6].textContent == slots[7].textContent && slots[7].textContent == slots[8].textContent && slots[8].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
     // if left column matches
     else if (slots[0].textContent == slots[3].textContent && slots[3].textContent == slots[6].textContent && slots[6].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
     // if middle column matches
     else if (slots[1].textContent == slots[4].textContent && slots[4].textContent == slots[7].textContent && slots[7].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
      // if right column matches
     else if (slots[2].textContent == slots[5].textContent && slots[5].textContent == slots[8].textContent && slots[8].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
     // if slash matches
     else if (slots[0].textContent == slots[4].textContent && slots[4].textContent == slots[8].textContent && slots[4].textContent != "") {
          // players wins
         win = true
-        removeClick(slots, player)
     }
     // if backslash matches
     else if (slots[6].textContent == slots[4].textContent && slots[4].textContent == slots[2].textContent && slots[4].textContent != "") {
          // players wins
-        win = true
-        removeClick(slots, player)
+        win = true   
     } 
+    else if (slots[0].textContent != "" && slots[1].textContent != "" && slots[2].textContent != "" 
+            && slots[3].textContent != "" && slots[4].textContent != "" && slots[5].textContent != "" 
+            && slots[6].textContent != "" && slots[7].textContent != "" && slots[8].textContent != "" ) {
+        win = "stalemate"
+    }
     else {
         console.log(win)
     }
     return win
-
     // else keep playing
 }
 
